3136a378c610a43fa67ef89ba51e1de69e31d6d9
TimSalokat <Tim.salokat@outlook.com>
2025-07-28 09:52:21 +0200

    Added api route to remove a route

--- handler/apihandler.go
+++ handler/apihandler.go
@@ -9,6 +9,22 @@ import (
 
 func ApiHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
+	case http.MethodGet:
+		println("Received GET request")
+		var routes []db.Route
+		result := db.Client.Find(&routes)
+		if result.Error != nil {
+			println("Error fetching routes from DB:", result.Error.Error())
+			http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+			return
+		}
+		println("Fetched routes count:", len(routes))
+		if err := json.NewEncoder(w).Encode(routes); err != nil {
+			println("Error encoding response:", err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
 	case http.MethodPost:
 		println("Received POST request")
 		var route db.Route
@@ -26,21 +42,30 @@ func ApiHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		println("Created route:", route.Domain)
 		w.WriteHeader(http.StatusCreated)
-	case http.MethodGet:
-		println("Received GET request")
-		var routes []db.Route
-		result := db.Client.Find(&routes)
+
+	case http.MethodDelete:
+		println("Received DELETE request")
+		type DeleteBody struct {
+			Domain string
+		}
+
+		var delBody DeleteBody
+		if err := json.NewDecoder(r.Body).Decode(&delBody); err != nil {
+			println("Error decoding request body:", err.Error())
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		println("Decoded route for deletion:", delBody.Domain)
+		result := db.Client.Where("domain = ?", delBody.Domain).Delete(&db.Route{})
 		if result.Error != nil {
-			println("Error fetching routes from DB:", result.Error.Error())
+			println("Error deleting route in DB:", result.Error.Error())
 			http.Error(w, result.Error.Error(), http.StatusInternalServerError)
 			return
 		}
-		println("Fetched routes count:", len(routes))
-		if err := json.NewEncoder(w).Encode(routes); err != nil {
-			println("Error encoding response:", err.Error())
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+		delete(db.MemoryRoutes, delBody.Domain)
+		println("Deleted route:", delBody.Domain)
+		w.WriteHeader(http.StatusOK)
+
 	default:
 		println("Received unsupported method:", r.Method)
 		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
